21425f4f6960a980c0337586a85e1cd2f34561d1
Krisztian Herbatenyi <k.herbatenyi@sparks-gmbh.de>
2025-05-26 09:35:54 +0200

    bra

--- polygon.py
+++ polygon.py
@@ -91,7 +91,6 @@ class Polygon:
         
     # Hier werden geparkte Autos zufällig einem Parkplatz zugeordnet
     def park_cars(self, surface):
-        attribut = "wird nie benutzt"
         # Definition der Parkplätze
         parkingSpacesX = []  # Liste für die X-Koordinaten der Parkplätze
         parkingSpacesY = []  # Liste für die Y-Koordinaten der Parkplätze
